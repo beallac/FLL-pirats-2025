5932921bdea9bfb3dc2571c3168cfa3fee16596b
team_member_name <team_member_name@example.com>
2025-10-27 17:22:44 -0700

    Added another mission and formated

--- mission_east_west.py
+++ mission_east_west.py
@@ -13,26 +13,19 @@ def run(br: BaseRobot):
     """
 
     br.driveForDistance(distance=25, speedPct=80, waiting=True)
-
-    br.turnInPlace(angle=-52, speedPct=45, waiting=True)
-
-    br.driveForDistance(distance=415, speedPct=80, waiting=True)
-
-    br.moveLeftAttachmentMotorForDegrees(degrees=-150, speedPct=150, waiting=True)
-
-    br.turnInPlace(angle=-33.75, speedPct=45, waiting=True)
+    br.turnInPlace(angle=-34.5, speedPct=45, waiting=True)
+    br.driveForDistance(distance=285, speedPct=80, waiting=True)
+    br.turnInPlace(angle=-26, speedPct=45, waiting=True)
+    br.driveForDistance(distance=155, speedPct=80, waiting=True)
+    br.moveLeftAttachmentMotorForDegrees(degrees=-250, speedPct=150, waiting=True)
+    br.turnInPlace(angle=-23.5, speedPct=45, waiting=True)
 
     br.driveForDistance(distance=700, speedPct=80, waiting=True)
-
     br.turnInPlace(angle=55, speedPct=45, waiting=True)
-
     br.moveLeftAttachmentMotorForDegrees(degrees=250, speedPct=150, waiting=True)
-
     br.turnInPlace(angle=-65, speedPct=45, waiting=True)
-
     br.driveForDistance(distance=630, speedPct=80, waiting=True)
-
-    br.moveLeftAttachmentMotorForDegrees(degrees=-100, speedPct=150, waiting=True)
+    br.moveLeftAttachmentMotorForDegrees(degrees=150, speedPct=150, waiting=True)
 
 
 # If running this file directly (for testing)
